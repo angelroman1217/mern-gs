d8b7f301b35fe4ba6becd620ced737535d3393d8
angelroman <angelromanmejia@hotmail.com>
2024-06-17 08:22:37 -0600

    sec commit

--- src/db.js
+++ src/db.js
@@ -1,10 +1,14 @@
 import mongoose from "mongoose";
+const DB_USER = "admin";
+const DB_PASS = "admin123";
+const DB_HOST = "web-personal.vyaut1x.mongodb.net";
 
 export const connectDB = async () => {
-    try {
-        await mongoose.connect(`mongodb+srv://<angel>:<testing123>@web-personal.vyaut1x.mongodb.net/?retryWrites=true&w=majority&appName=web-personal`);
-        console.log('DB is connected');
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    const uri = `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/`;
+    await mongoose.connect(uri);
+    console.log('DB is connected');
+  } catch (error) {
+    console.log(error);
+  }
 }
